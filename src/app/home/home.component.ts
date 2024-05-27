94b82556b26d83b74aeb11a6cece3c7648b6f11f
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-27 19:19:38 +0200

    .set(), .update(), .asReadonly() methods for signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,7 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
 @Component({
   selector: 'home',
   standalone: true,
+  //signals: true, //'signals' does not exist in type 'Component'.
   imports: [
     MatTabGroup,
     MatTab,
@@ -20,16 +21,20 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  counter = 0;
+  //counter = 0;
   counterSignal = signal(0);
+  readOnlySignal = signal(5).asReadonly(); //read only signal
 
 
-  increment() {
+  /*increment() {
     this.counter++;
-  }
+  }*/
 
   incrementSignal() {
-    this.counterSignal.set(this.counterSignal()+1);
+    //this.counterSignal.set(this.counterSignal()+1);
+    
+    //here we take current value and adding 1 to this current value
+    this.counterSignal.update((counter) => counter +1);
   }
 
 }
