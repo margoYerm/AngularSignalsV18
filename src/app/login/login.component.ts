6a7d316b34d192c0c07b2d03240353218ef970cf
Margo Yermachenko <margoermachenko@gmail.com>
2024-06-07 20:05:42 +0200

    Implemented authentication service (login, logout, save profile to the local storage) with shared login component; added logic for show/hide login/logout item in the menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import {Component, inject} from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../services/auth.service";
 import {MessagesService} from "../messages/messages.service";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'login',
@@ -16,5 +17,34 @@ import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 })
 export class LoginComponent {
 
+  fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    email: ['', Validators.required], 
+    password: ['', Validators.required],
+  });
+
+  messagesService = inject(MessagesService);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  async onLogin() {
+    try {
+      const {email, password} = this.form.value;
+      if (!email || !password) {
+        this.messagesService.showMessage(
+          'Enter an email and password.',
+          'error');
+        return;
+      }
+      await this.authService.login(email, password);      
+      await this.router.navigate(['/home']);
+    } 
+    catch(err) {
+      console.error(err);
+      this.messagesService.showMessage(
+        'Login failed... Please, try again', 
+        'error')
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,57 @@ import {computed, effect, inject, Injectable, signal} from "@angular/core";
 import {User} from "../models/user.model";
 import {environment} from "../../environments/environment";
 import {Router} from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { firstValueFrom } from "rxjs";
+import { json } from "body-parser";
 
-const USER_STORAGE_KEY = 'user';
+const USER_STORAGE_KEY = 'user'; //for accept local storage
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  #userSignal = signal<User | null>(null);
+  user = this.#userSignal.asReadonly();
+  isLoggedIn = computed(() => !!this.user());
 
+  http = inject(HttpClient);
+  router = inject(Router); //for redirect user after logout
 
+  //for create effect, witch takes user key from local storage
+  constructor() {
+    //to load user profile from storage
+    this.loadUserFromStorage();
+    //we will detect when user profile is available
+    effect(() => {
+      const user = this.user();
+      //save user profile to local storage
+      if(user) {
+        localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+      }
+    });
+  }
+
+  //load user profile from local storage if app will restarted
+  loadUserFromStorage() {
+    const json = localStorage.getItem(USER_STORAGE_KEY);
+    if (json) {
+      const user = JSON.parse(json);
+      this.#userSignal.set(user);
+    }
+  }
+
+  async login(email: string, password: string): Promise<User> {
+    const login$ = this.http.post<User>(`${environment.apiRoot}/login`,
+      {email, password});
+    const user = await firstValueFrom(login$);
+    this.#userSignal.set(user);
+    return user;
+  }
+
+  async logout() {
+    localStorage.removeItem(USER_STORAGE_KEY);
+    this.#userSignal.set(null);
+    await this.router.navigateByUrl('/login');
+  }
 }
