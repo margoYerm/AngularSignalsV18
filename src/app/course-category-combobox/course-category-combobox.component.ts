c0e3495238ac3b41f1cc0721292167cdcd625953
Margo Yermachenko <margoermachenko@gmail.com>
2024-06-11 23:59:49 +0200

    How works content projection with signals. viewChild, viewChildren

--- src/app/course-category-combobox/course-category-combobox.component.ts
+++ src/app/course-category-combobox/course-category-combobox.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, model} from '@angular/core';
+import {Component, ContentChild, ElementRef, contentChild, effect, input, model} from '@angular/core';
 import {CourseCategory} from "../models/course-category.model";
 
 @Component({
@@ -10,7 +10,8 @@ import {CourseCategory} from "../models/course-category.model";
 })
 export class CourseCategoryComboboxComponent {
   //we will pass initial value to this input
-  label = input.required<string>() //label of the dropdown
+  //label = input.required<string>() //label of the dropdown
+  title = contentChild.required<ElementRef>('title');
 
   //Model input (value) has the value of selected item (course category)
   //this works as an input and as an output
@@ -19,6 +20,9 @@ export class CourseCategoryComboboxComponent {
   constructor() {
     //this.label(); //it's input readonly 
     //this.value(); //can be set or update - writable signal
+    effect(() => {
+      console.log('Content child El ', this.title());
+    })
   }
 
   onCategoryChanged(category: string) {
