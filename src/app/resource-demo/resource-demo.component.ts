167e833dc0422e09ff40cc8f2e02425b6a2cd4f1
Margo Yermachenko <margoermachenko@gmail.com>
2025-01-11 16:44:05 +0100

    Added an example with using resource() signal

--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -15,9 +15,40 @@ export class ResourceDemoComponent {
 
   search = signal<string>('');
 
-  
+  //empty signals array
+  //results that wa are getting from the backend
+  //lessons = signal<Lesson[]>([]);
 
-  searchLessons(search: string) {}
+  //generics: lesson[] - result that we expect, {search: } data from the search string for fetch 
+  lessons = resource<Lesson[], {search: string}>({
+    //determine when the backend request is going to be made. 
+    //when a new value in search will be emitted, request should be reloaded
+    request: () => ({
+      search: this.search() //possible multiple properties / src signals
+    }),
+    //to fetching 
+    //abortSignal - fn for fetch that perform requests 
+    loader: async ({request, abortSignal}) => {
+      const response = await fetch(`${this.env.apiRoot}/search-lessons?query=${request.search}&courseId=18`, {
+        signal: abortSignal
+      });
+      const json = await response.json();
+      return json.lessons;
+    }
+  });
+
+  constructor() {
+    //we have many options for lessons when it is resource this.lessons.
+
+    //this emits the search string as the user types it
+    effect(() => {
+      console.log(`searching lessons: `, this.search());
+    })
+  }
+
+  searchLessons(search: string) {
+    this.search.set(search);
+  }
 
   reload() {}
 
